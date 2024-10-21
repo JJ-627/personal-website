8c6d72b237a4f4b11d569ef00cc0abbb4748ecc4
JJ-627 <JefferyJJ627@gmail.com>
2024-10-22 01:02:33 +0800

    Add Animation, and update Resume

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -107,15 +107,11 @@ $(document).ready(function(){
 
     // 5. welcome animation support
 
-        $(window).load(function(){
-        	$(".header-text h2,.header-text p").removeClass("animated fadeInUp").css({'opacity':'0'});
-            $(".header-text a").removeClass("animated fadeInDown").css({'opacity':'0'});
-        });
-
-        $(window).load(function(){
-        	$(".header-text h2,.header-text p").addClass("animated fadeInUp").css({'opacity':'0'});
-            $(".header-text a").addClass("animated fadeInDown").css({'opacity':'0'});
-        });
+	$(window).load(function(){
+		setTimeout(function() {
+			$(".header-text").addClass("floating");
+		}, 1800);
+	});
 
 });	
 	
